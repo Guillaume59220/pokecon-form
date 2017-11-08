b845472fd86628145f29b8a4ede39b997eca37f5
Guillaume-Batailler <guillaumedu59220@hotmail.com>
2017-11-08 16:09:05 +0100

    modif de 16:10

--- js/function.js
+++ js/function.js
@@ -1,31 +1,29 @@
-16 lines (15 sloc) 619 Bytes 
-
 function changePokemon(event) {
-
-  selPokemon = $(this).val();
-
-  // Récupération de la valeur name de l'élément courant
-
-  select = $(this).attr('name');
-
-  // Si aucun pokemon n'est sélectionné
-
-  if (selPokemon == '') {
-
-    $("[name='pv_" + select + "']").val(null);
-
-    $("[name='defense_" + select + "']").val(null);
-
-    $("[name='attaque_" + select + "']").val(null);
-
-  } else {
-
-    $("[name='pv_" + select + "']").val(pokemons[selPokemon]["pv"]);
-
-    $("[name='defense_" + select + "']").val(pokemons[selPokemon]["defense"]);
-
-    $("[name='attaque_" + select + "']").val(pokemons[selPokemon]["attaque"]);
-
+  
+    selPokemon = $(this).val();
+  
+    // Récupération de la valeur name de l'élément courant
+  
+    select = $(this).attr('name');
+  
+    // Si aucun pokemon n'est sélectionné
+  
+    if (selPokemon == '') {
+  
+      $("[name='pv_" + select + "']").val(null);
+  
+      $("[name='defense_" + select + "']").val(null);
+  
+      $("[name='attaque_" + select + "']").val(null);
+  
+    } else {
+  
+      $("[name='pv_" + select + "']").val(pokemons[selPokemon]["pv"]);
+  
+      $("[name='defense_" + select + "']").val(pokemons[selPokemon]["defense"]);
+  
+      $("[name='attaque_" + select + "']").val(pokemons[selPokemon]["attaque"]);
+  
+    }
+  
   }
-
-}
--- test/db.php
+++ test/db.php
@@ -1,195 +1,139 @@
-<!doctype html>
-
-<html lang="fr">
-
-<head>
-
-  <meta charset="utf-8">
-
-
-
-  <title>Formulaire d'insertion en base de donnée</title>
-
-
-
-  <link rel="stylesheet" href="../css/style.css">
-
-
-
-  <!--[if lt IE 9]>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-
-  <![endif]-->
-
-</head>
-
-
-
-<body>
-
-
-
 <?php
 
-  require_once('../inc/function.php');
+/*
 
+ * Script de test pour la connexion Base de donnée : BDD
 
+ */
 
-  $errors = [];
+// Configuration de la base de données à placer dans un fichier différent pour la production
 
+define('HOST', 'localhost'); // Domaine ou IP du serveur ou est située la base de données
 
+define('USER', 'root'); // Nom d'utilisateur autorisé à se connecter à la base
 
-  // Configuration de la base de données à placer dans un fichier différent pour la production
+define('PASS', ''); // Mot de passe de connexion à la base
 
-  define('HOST', 'localhost'); // Domaine ou IP du serveur ou est située la base de données
+define('DB', 'pokemon'); // Base de données sur laquelle on va faire les requêtes
 
-  define('USER', 'root'); // Nom d'utilisateur autorisé à se connecter à la base
 
-  define('PASS', ''); // Mot de passe de connexion à la base
 
-  define('DB', 'pokemon'); // Base de données sur laquelle on va faire les requêtes
+$db_options = array(
 
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,// On affiche des warnings pour les erreurs, à commenter en prod (valeur par défaut PDO::ERRMODE_SILENT)
 
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC   // Mode ASSOC par défaut pour les fetch
 
-  $db_options = array(
+);
 
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,// On affiche des warnings pour les erreurs, à commenter en prod (valeur par défaut PDO::ERRMODE_SILENT)
 
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC   // Mode ASSOC par défaut pour les fetch
 
-  );
 
 
+echo "<pre>";
 
-  $dsn = 'mysql:host=' . HOST . ';dbname=' . DB;
+$dsn = 'mysql:host=' . HOST . ';dbname=' . DB;
 
-  try {
+try {
 
-    $db = new PDO($dsn, USER, PASS, $db_options);
+  $db = new PDO($dsn, USER, PASS, $db_options);
 
-  } catch (PDOException $e) {
+} catch (PDOException $e) {
 
-    $errors[] = "Erreur de connexion : " . $e->getMessage();
+  die("Erreur de connexion : " . $e->getMessage());
 
-  }
+}
 
 
 
-  if (formIsSubmit('insertPokedex')) {
+echo "A partir d'ici nous sommes connectés\n\n";
 
-    // code d'insertion
 
-    echo "<p>Nouvel enregistrement</p>";
 
-    $nom_proprietaire = $_POST['nom_proprietaire'];
+// Lister les pokedex enregistrés
 
+$query = $db->query('SELECT * FROM pokedex');
 
+$result = $query->fetch();
 
-    // Validation
+echo "result : ";
 
-  }
+var_dump($result);
 
 
 
-  // Lister les pokedex enregistrés
+// Affichage d'un tableau PHP en tableau html
 
-  if (!$query = $db->query('SELECT * FROM pokedex')) {
+$table = "
 
-    $errors[] = "Erreur lors de la création de la requête";
+  <table style='border-collapse: collapse;'>
 
-  }
+    <thead>
 
-  if(!$result = $query->fetch()) {
+      <tr>
 
-    $errors[] = "Aucune ligne trouvée";
+        <th style='border: solid;'>
 
-  }
+        " . implode('</th><th style="border: solid;">', array_keys($result)) . "
 
+        </th>
 
+      </tr>
 
-  if (count($errors) == 0) {
+    </thead>
 
-    // Affichage d'un tableau PHP en tableau html
+    <tbody>
 
-    $table = "
+      <tr>
 
-      <table style='border-collapse: collapse;'>
+        <td style='border: solid;'>
 
-        <thead>
+        " . implode('</td><td style="border: solid;">', $result) . "
 
-          <tr>
+        </td>
 
-            <th style='border: solid;'>
+      </tr>
 
-            " . implode('</th><th style="border: solid;">', array_keys($result)) . "
+    </tbody>
 
-            </th>
+  </table>
 
-          </tr>
+";
 
-        </thead>
 
-        <tbody>
 
-          <tr>
+echo "</pre>$table<pre>";
 
-            <td style='border: solid;'>
 
-            " . implode('</td><td style="border: solid;">', $result) . "
 
-            </td>
+if (!$query = $db->query("INSERT INTO pokedex(nom_proprietaire) VALUES('Sacha')")) {
 
-          </tr>
+  echo 'Erreur de requête : ';
 
-        </tbody>
+  var_dump($db->errorInfo());
 
-      </table>
+  return 0;
 
-    ";
+}
 
-  }
+$query->execute();
+
+
+
+$query = $db->query('SELECT * FROM pokedex');
+
+$result = $query->fetch();
+
+echo "result : ";
+
+var_dump($result);
+
+
+
+echo "</pre>";
+
+
+
+
 
 ?>
-
-
-
-  <form method="post">
-
-    <input type="hidden" name="insertPokedex" value="1"/>
-
-
-
-    <label for="nom_proprietaire">Nom du proprietaire : </label>
-
-    <input id="nom_proprietaire" name="nom_proprietaire" type="text"/>
-
-
-
-    <button type="submit">Ajouter</button>
-
-  </form>
-
-
-
-
-
-<?php
-
-  if (count($errors) > 0)
-
-    echo "<p>" . implode("</p><p>", $errors) . "</p>";
-
-  else
-
-    echo "$table";
-
-?>
-
-
-
-  <!--script src="js/scripts.js"></script-->
-
-</body>
-
-</html>
